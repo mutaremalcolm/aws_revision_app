2d6c682f92b5e6c9b149f970b9c458232bb54129
“malcolmmutare” <mutarem@gmail.com>
2025-01-13 12:12:00 +0200

    feat: fully implemented google analytics functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Analytics />
     </div>
   );
 }
